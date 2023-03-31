e06502b8ba95eeacb489eae18c25db3040ce8540
Fachri Hawari <fachri.hawari@gmail.com>
2023-03-31 16:19:56 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'expo-dev-client';
+import React, { useState } from 'react';
+import AgoraUIKit from 'agora-rn-uikit';
+import { Button, View, TextInput, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [videoCall, setVideoCall] = useState(false);
+  const [username, setUsername] = useState("")
+
+  /**
+   * @type {import('agora-rn-uikit').ConnectionData}
+   */
+  const connectionData = {
+    appId: 'app-id-from-agora',
+    channel: 'extraordinary-channel',
+    username: username
+  }
+
+  /**
+   * @type {import('agora-rn-uikit').rtmCallbacks}
+   */
+  const rtcCallbacks = {
+    EndCall: () => {
+      setTimeout(() => {
+        setVideoCall(false)
+      }, 200);
+    },
+  }
+
+  /**
+   * @type {import('agora-rn-uikit').Settings}
+   */
+  const settings = {
+    displayUsername: true,
+  }
+
+  const handleJoin = () => {
+    setVideoCall(true)
+  }
+
+  const handleNameChange = (username) => {
+    setUsername(username)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {videoCall ? (
+        <AgoraUIKit settings={settings} connectionData={connectionData} rtcCallbacks={rtcCallbacks} />
+      ) : (
+        <View style={styles.form}>
+          <TextInput placeholder='Isi Username' style={styles.input} onChangeText={handleNameChange} />
+          <Button onPress={handleJoin} title='Join' />
+        </View>
+      )}
     </View>
-  );
-}
+  )
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
-});
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  input: {
+    borderWidth: 1,
+    width: 200,
+    height: 36,
+    marginRight: 8,
+    paddingHorizontal: 8
+  }
+})
---
+++ README.md
@@ -0,0 +1,42 @@
+# Agora Integration
+
+
+## 1. Install eas cli
+
+```bash
+npm install --global eas-cli
+```
+
+## 2. Install deps
+
+```bash
+npm i agora-react-native-rtm agora-rn-uikit expo-dev-client react-native-agora
+```
+
+## 3. Create & set expo project to app.json
+
+```bash
+eas init
+# kalo dapet pertanyaan gini > Would you like to create a project for @fachrihawari/hck-55-agora-2? › (Y/n)
+# tekan enter aja
+```
+
+## 4. Init build configuration
+
+```bash
+eas build:configure
+```
+
+## 5. Build custom expo go untuk development
+
+```bash
+eas build --platform android --profile development
+
+# ketika jalanin command diatas akan dapet beberapa pertanyaan, cukup teken enter enter aja
+# 📝  Android application id Learn more
+# ✔ What would you like your Android application id to be? … com.fachrihawari.hck55agora2
+# ✔ Using remote Android credentials (Expo server)
+# ✔ Generate a new Android Keystore? … yes
+# ✔ Created keystore
+
+```
